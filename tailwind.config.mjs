b6f6f3a472dd4614adab435979da1d6d301d2528
gdejules <gd.juliarthaa@gmail.com>
2026-05-01 20:00:00 +0800

    Add new product section and styling in index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -6,6 +8,9 @@ export default {
       cream: "#EDE99A",
       orange: "#EA7601",
       green: "#258225",
+      white: "#FFFFFF",
+      black: "#000000",
+      amber: colors.amber,
     },
   },
   plugins: [],
