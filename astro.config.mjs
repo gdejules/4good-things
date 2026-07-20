0fcae4a2e3e5a49c7cb9ada20efefbf4bd98201c
gdejules <gd.juliarthaa@gmail.com>
2026-07-20 17:46:54 +0800

    Delete some font files and set up new fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -8,4 +8,46 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "NeoSansPro",
+      cssVariable: "--font-neo-sans",
+      fallbacks: ["sans-serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/NeoSansProCyr-Regular.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+          {
+            src: ["./src/fonts/NeoSansProCyr-Medium.woff2"],
+            weight: "medium",
+            style: "normal",
+          },
+          {
+            src: ["./src/fonts/NeoSansProCyr-Bold.woff2"],
+            weight: "bold",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "BobbyJonesSoft",
+      cssVariable: "--font-bobby-jones",
+      fallbacks: ["sans-serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/BobbyJonesSoft-Regular.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
