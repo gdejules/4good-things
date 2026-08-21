13dad369147f433564c48b462720bd500f31bd82
gdejules <gd.juliarthaa@gmail.com>
2026-08-21 10:36:18 +0800

    Adjust new NavBar layout for responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -73,77 +73,9 @@ const closeSliderButton = {
 
 export default function Navigation({ logoImage }) {
   return (
-    <nav className="section py-custom-xs-s bg-warm-alabaster">
-      <span id="nav-label" hidden>
-        Navigation
-      </span>
-      <li
-        id="btnOpen"
-        className="topnav-open md:hidden"
-        aria-expanded="false"
-        aria-labelledby="nav-label">
-        <HamburgerMenu
-          active={hamburgerButton.active}
-          size={hamburgerButton.size}
-        />
-      </li>
-      <div
-        className="topnav-menu md:hidden fixed inset-0 p-6 bg-soft-sand"
-        role="dialog"
-        aria-labelledby="nav-label">
-        <CloseMenuSlider
-          active={closeSliderButton.active}
-          size={closeSliderButton.size}
-          onPress={closeSliderButton.onPress}
-          className="topnav-close block ms-auto mbe-32"
-        />
-        <ul className="topnav-links flex flex-col justify-center items-center gap-custom-2xs-xs">
-          <li>
-            <SecondaryButton
-              href={mobileHomeButton.href}
-              active={mobileHomeButton.active}
-              size={mobileHomeButton.size}>
-              {mobileHomeButton.children}
-            </SecondaryButton>
-          </li>
-          <li>
-            <SecondaryButton
-              href={productButton.href}
-              active={productButton.active}
-              size={productButton.size}>
-              {productButton.children}
-            </SecondaryButton>
-          </li>
-          <li>
-            <SecondaryButton
-              href={projectButton.href}
-              active={projectButton.active}
-              size={projectButton.size}>
-              {projectButton.children}
-            </SecondaryButton>
-          </li>
-          <li>
-            <SecondaryButton
-              href={aboutButton.href}
-              active={aboutButton.active}
-              size={aboutButton.size}>
-              {aboutButton.children}
-            </SecondaryButton>
-          </li>
-          <li>
-            <SocialButton
-              before={<Whatsapp />}
-              sharer={contactButton.sharer}
-              type={contactButton.type}
-              active={contactButton.active}
-              size="small"
-              className="mobile-nav"
-            />
-          </li>
-        </ul>
-      </div>
-      <ul className="desktop-nav max-w-6xl mx-auto flex justify-between items-center max-md:mx-6 max-xl:mx-10">
-        <li className="max-lg:hidden">
+    <header className="section py-custom-xs-s bg-warm-alabaster">
+      <div className="max-w-6xl mx-auto flex justify-between items-center max-md:mx-6 max-xl:mx-10">
+        <div className="max-lg:hidden">
           <SecondaryButton
             href={homeButton.href}
             active={homeButton.active}
@@ -156,8 +88,8 @@ export default function Navigation({ logoImage }) {
               class="w-auto"
             />
           </SecondaryButton>
-        </li>
-        <li className="lg:hidden">
+        </div>
+        <div className="lg:hidden">
           <SecondaryButton
             href={homeButton.href}
             active={homeButton.active}
@@ -170,55 +102,81 @@ export default function Navigation({ logoImage }) {
               class="w-auto"
             />
           </SecondaryButton>
-        </li>
-        <ul className="flex justify-between items-center gap-custom-2xs-xs max-md:hidden">
-          <li>
-            <SecondaryButton
-              href={productButton.href}
-              active={productButton.active}
-              size={productButton.size}>
-              {productButton.children}
-            </SecondaryButton>
-          </li>
-          <li>
-            <SecondaryButton
-              href={projectButton.href}
-              active={projectButton.active}
-              size={projectButton.size}>
-              {projectButton.children}
-            </SecondaryButton>
-          </li>
-          <li>
-            <SecondaryButton
-              href={aboutButton.href}
-              active={aboutButton.active}
-              size={aboutButton.size}>
-              {aboutButton.children}
-            </SecondaryButton>
-          </li>
-        </ul>
-        <li className="max-lg:hidden">
-          <SocialButton
-            before={<Whatsapp />}
-            sharer={contactButton.sharer}
-            type={contactButton.type}
-            active={contactButton.active}
-            size={contactButton.size}
-            className="nav">
-            {contactButton.children}
-          </SocialButton>
-        </li>
-        <li className="lg:hidden max-md:hidden">
-          <SocialButton
-            before={<Whatsapp />}
-            sharer={contactButton.sharer}
-            type={contactButton.type}
-            active={contactButton.active}
-            size="small"
-            className="mobile-nav"
-          />
-        </li>
-      </ul>
-    </nav>
+        </div>
+        <nav className="">
+          <span id="nav-label" hidden>
+            Navigation
+          </span>
+          <div
+            id="btnOpen"
+            className="topnav-open md:hidden"
+            aria-expanded="false"
+            aria-labelledby="nav-label">
+            <HamburgerMenu
+              active={hamburgerButton.active}
+              size={hamburgerButton.size}
+            />
+          </div>
+          <div
+            className="topnav-menu md:hidden fixed inset-0 p-6 bg-soft-sand"
+            role="dialog"
+            aria-labelledby="nav-label">
+            <CloseMenuSlider
+              active={closeSliderButton.active}
+              size={closeSliderButton.size}
+              onPress={closeSliderButton.onPress}
+              className="topnav-close block ms-auto mbe-32"
+            />
+            <ul className="topnav-links flex max-md:flex-col justify-center items-center gap-custom-xs-s">
+              <li>
+                <SecondaryButton
+                  href={productButton.href}
+                  active={productButton.active}
+                  size={productButton.size}>
+                  {productButton.children}
+                </SecondaryButton>
+              </li>
+              <li>
+                <SecondaryButton
+                  href={projectButton.href}
+                  active={projectButton.active}
+                  size={projectButton.size}>
+                  {projectButton.children}
+                </SecondaryButton>
+              </li>
+              <li>
+                <SecondaryButton
+                  href={aboutButton.href}
+                  active={aboutButton.active}
+                  size={aboutButton.size}>
+                  {aboutButton.children}
+                </SecondaryButton>
+              </li>
+              <li className="max-lg:hidden">
+                <SocialButton
+                  before={<Whatsapp />}
+                  sharer={contactButton.sharer}
+                  type={contactButton.type}
+                  active={contactButton.active}
+                  size={contactButton.size}
+                  className="nav">
+                  {contactButton.children}
+                </SocialButton>
+              </li>
+              <li className="lg:hidden">
+                <SocialButton
+                  before={<Whatsapp />}
+                  sharer={contactButton.sharer}
+                  type={contactButton.type}
+                  active={contactButton.active}
+                  size="small"
+                  className="mobile-nav"
+                />
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 }
